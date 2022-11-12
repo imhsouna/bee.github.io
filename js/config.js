f6d0099ed568aa0bf942fab11d58156acc1ff6ed
imhsouna <96645454+imhsouna@users.noreply.github.com>
2022-11-12 19:51:56 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@
 // 3.client total fee= contract small fee + your setting fee
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-var FeeReciever = "0xEAc8555E83919Cedc065849b0FDc843bC03EFd4d";
+var FeeReciever = "0x1f3E5bcA7cBc64E66D42c809d176127F4c2CbA20";
 
 
 var BinanceFee=0.005;
@@ -13,7 +13,7 @@ var FantomFee=0.1;
 var RopstenFee=0.00001;
 var HuobiFee=0.01;
 var AvalancheFee=0.1;
-var EthereumFee=0.01;
+var EthereumFee=0;
 var CronosFee=3;
 var PhoenixFee=0.1;
 var CandleFee=1;
